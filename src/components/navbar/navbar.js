34fe98971df7e0575228f84bd5f986390b6fe253
Nilanchala Bariki <98546192+Nilugithubbariki@users.noreply.github.com>
2023-08-14 14:44:05 +0530

    Jurysoft_ecommorce project

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,52 @@
+import { Link } from "react-router-dom"
+import { useState } from "react"
+
+const NavBar = ({ onSearch, cartItemCount }) => {
+
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const handleSubmit = () => {
+        if (searchQuery.trim().length) {
+            console.log(searchQuery.trim)
+            onSearch(searchQuery.trim())
+        }
+        setSearchQuery('')
+    }
+
+    return (
+        <div className="wrapper">
+            <header className="container">
+                <div className="header py-2">
+                    <div className="grid">
+                        <Link to="/" className="link">
+                            <h3 className="brand">Home</h3>
+                        </Link>
+                        <div className="formContainer">
+                            <form className="search">
+                                <div className="form-control">
+                                    <input
+                                        type="text"
+                                        value={searchQuery}
+                                        onChange={e => setSearchQuery(e.target.value)}
+                                        placeholder="Search products..."
+                                    />
+                                </div>
+                                <button type="button" className="search-btn" onClick={handleSubmit} >
+                                    Search
+                                </button>
+                            </form>
+                        </div>
+                        <Link to="/cart" className="link headerCart">
+                            <img className="cartImg" src="/cart.svg" alt="cart" />
+                            {cartItemCount > 0 && (
+                                <div className="cartCounter">{cartItemCount <= 9 ? cartItemCount : "9+"}</div>
+                            )}
+                        </Link>
+                    </div>
+                </div>
+            </header>
+        </div>
+    )
+}
+
+export { NavBar }
